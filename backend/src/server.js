2a19cd94f7a8325cdc6cdc60a8aee85277f2e56d
mdfaiz9 <mdfaizalam827@gmail.com>
2025-10-08 09:48:30 +0530

    Update CORS and Axios base URL for production deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,8 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://snap-sync-frontend.vercel.app",
     credentials: true, // allow frontend to send cookies
   })
 );
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 const BASE_URL =
   import.meta.env.MODE === "development"
-    ? "http://localhost:8001/api"
-    : import.meta.env.VITE_API_URL || "/api"; // use custom var if available
+    ? "https://snap-sync-backend.vercel.app/api"
+    : import.meta.env.VITE_API_URL; // use custom var if available
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
