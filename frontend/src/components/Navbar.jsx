dd4541ac0f84ae4cb4e3f5d2608583e2675fb358
mdfaiz9 <mdfaizalam827@gmail.com>
2025-10-08 14:22:01 +0530

    Refactor logout button to use an inline function for logout mutation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -55,7 +55,10 @@ const Navbar = () => {
           </div>
 
           {/* Logout button */}
-          <button className="btn btn-ghost btn-circle" onClick={logoutMutation}>
+          <button
+            className="btn btn-ghost btn-circle"
+            onClick={() => logoutMutation()}
+          >
             <LogOutIcon className="h-6 w-6 text-base-content opacity-70" />
           </button>
         </div>
